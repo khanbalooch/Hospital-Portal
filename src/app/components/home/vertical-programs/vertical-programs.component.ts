92d7793a2a5f008a548f37c0d0b2976315c16a35
tayyab4web <dev.tayyab@gmail.com>
2018-03-10 23:51:28 +0500

    vert-prog ts

--- src/app/components/home/vertical-programs/vertical-programs.component.ts
+++ src/app/components/home/vertical-programs/vertical-programs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-declare var jquery:any;
-declare var $ :any;
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-vertical-programs',
@@ -12,44 +12,44 @@ export class VerticalProgramsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+
     $(document).ready(function () {
       $('.itemPanel').slick({
-          infinite: true,
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          //autoplay: true,
-          //autoplaySpeed: 3000,
-          pauseOnHover: true,
-          responsive: [
-              {
-                  breakpoint: 1080,
-                  settings: {
-                      slidesToShow: 3,
-                      slidesToScroll: 1,
-                      infinite: true,
-                  }
-              },
-              {
-                  breakpoint: 840,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true
-                  }
-                },
-                {
-                  breakpoint: 580,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true
-                  }
-                }
-          ]
-  
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        //autoplay: true,
+        //autoplaySpeed: 3000,
+        pauseOnHover: true,
+        responsive: [
+          {
+            breakpoint: 1080,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 840,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true
+            }
+          },
+          {
+            breakpoint: 580,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true
+            }
+          }
+        ]
+
       });
-  });
+    });
 
 
   }
