4d7ffb42460529b8399723275a4ae4affc1780c8
tayyab4web <dev.tayyab@gmail.com>
2018-03-10 21:28:48 +0500

    header and responsive slick slider

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -6,7 +6,33 @@ $(document).ready(function () {
         slidesToScroll: 1,
         //autoplay: true,
         //autoplaySpeed: 3000,
-        pauseOnHover: true
-        
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1080,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 840,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                  infinite: true
+                }
+              },
+              {
+                breakpoint: 580,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true
+                }
+              }
+        ]
+
     });
 });
