7e7f4e7ac277aab5abeda3cc83491a12e4959ed2
khan <muh_ibrahim@outlook.com>
2018-03-10 21:33:30 +0500

    success response in contact form

--- src/app/components/contact/contact-form/contact-form.component.ts
+++ src/app/components/contact/contact-form/contact-form.component.ts
@@ -11,9 +11,12 @@ import { ValidationService } from '../../../shared/services/validation/validatio
 export class ContactFormComponent implements OnInit {
 
   form: FormGroup;
-
+  userName:string;
+  isHidden: boolean;
   constructor(private fb: FormBuilder, private afdb: AngularFireDatabase) {
     this.createForm();
+
+    this.isHidden = false;
    }
 
 //todo mobile Number validation
@@ -28,6 +31,7 @@ export class ContactFormComponent implements OnInit {
    }
 
    onSubmit() {
+     
       const {name, mobileNumber, email, message} = this.form.value;
       const date = Date();
       const html = `
@@ -39,6 +43,8 @@ export class ContactFormComponent implements OnInit {
       `;
       const formRequest = { name, mobileNumber, email, message, date, html };
       this.afdb.list('/messages').push(formRequest); // submit data to firebase*/
+      this.userName = name;
+      this.isHidden=true;
       this.form.reset(); // reset complete form todo
   }
   ngOnInit() {
